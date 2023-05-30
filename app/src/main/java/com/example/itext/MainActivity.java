d24410578325b8bd1fefb03649d455a04e658c43
Arijit2003 <arijitmodak2003@gmail.com>
2023-05-30 15:20:30 +0530

    checking already a file exists or not

--- app/src/main/java/com/example/itext/MainActivity.java
+++ app/src/main/java/com/example/itext/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.itext;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -28,17 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText pdfName;
     private Button btnCreate;
 
-//    ActivityResultLauncher<String> writeExternalStorage =
-//            registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-//                @Override
-//                public void onActivityResult(Boolean result) {
-//                    if(result){
-//
-//                    }else{
-//                        Toast.makeText(MainActivity.this, "Write External Storage permission denied", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
+
 
 
     @Override
@@ -81,15 +67,18 @@ public class MainActivity extends AppCompatActivity {
         }else{
             file = new File(pdfPath,pdfName.getText().toString()+".pdf");
         }
-        PdfWriter pdfWriter = new PdfWriter(file);
-        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
-        Document document = new Document(pdfDocument);
-        Paragraph paragraph = new Paragraph(Objects.requireNonNull(pdfContent.getText()).toString());
-        document.add(paragraph);
-        document.close();
+        if(!file.exists()) {
+            PdfWriter pdfWriter = new PdfWriter(file);
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            Document document = new Document(pdfDocument);
+            Paragraph paragraph = new Paragraph(Objects.requireNonNull(pdfContent.getText()).toString());
+            document.add(paragraph);
+            document.close();
 
-        Toast.makeText(MainActivity.this,pdfName.getText().toString()+" created successfully",Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(MainActivity.this, pdfName.getText().toString() + " created successfully", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, pdfName.getText().toString()+" already exists", Toast.LENGTH_SHORT).show();
+        }
 
 
 
